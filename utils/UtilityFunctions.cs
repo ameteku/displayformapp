ecf95f28faceec35d93b2d50930da2d0d4f76745
Michael Ameteku <sa_ma368@uakron.edu>
2022-09-07 13:40:55 -0400

    Add utility function file

---
+++ utils/UtilityFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayFormApp.utils
+{
+    public class UtilityFunctions 
+    {
+      public static String removeOpenAndClosingQoutationMarks(String stringObject)
+        {
+            if(stringObject.Length != 0)
+            {
+                if(stringObject[0] == '"')
+                {
+                    stringObject = stringObject.Remove(0, 1);
+                    if (stringObject[stringObject.Length-1] == '"')
+                    {
+                        stringObject = stringObject.Remove(stringObject.Length - 1, 1);
+                    }
+                }
+            }
+            return stringObject;
+        }
+    }
+}
