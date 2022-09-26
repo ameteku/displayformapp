99835df729785eada3a09180f1c46ce4bb711b7f
Michael Ameteku <sa_ma368@uakron.edu>
2022-09-26 19:46:55 -0400

    Create DatSourceHandler and restructure manual upload code

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using DisplayFormApp.DataSourceHandler;
 using DisplayFormApp.utils;
 
 namespace DisplayFormApp
@@ -5,121 +6,17 @@ namespace DisplayFormApp
     public partial class Form1 : Form
     {
         public List<Class> Classes { get; set; }
-        public List<Class> UnfilteredClasses { get; set; }
         private int currentDay;
+        private DataSourceWrapper dataSourceWrapper { get; set; }
         enum DataGridView1Headers { Subject, InstructorName, RoomNumber, StartTime, EndTime, Date }
 
-        public Form1()
+        public Form1(ref DataSourceWrapper wrapper)
         {
-            Classes = getClasses();
+            dataSourceWrapper = wrapper;
+            Classes = dataSourceWrapper.getClassesFunction();
             InitializeComponent();
         }
  
-        private List<Class> getClasses()
-        {
-
-            string path = Form2.ScheduleFolderPath; 
-           
-            string path101 = path + @"\Schedule101.CSV";
-            string path106 = path + @"\Schedule106.CSV";
-            string path176 = path + @"\Schedule176.CSV";
-
-            // Create Stream Reader
-            StreamReader reader = null;
-
-            List<Class> classes = new List<Class>();
-            List<Class> unfilteredClasses = new List<Class>();
-
-            string currentFilePath = null;
-            string currentRoomNumber = null;
-
-            for (int i = 0; i < 3; i++)
-            {
-                switch (i)
-                {
-                    case 0:
-                        currentFilePath = path101;
-                        currentRoomNumber = "101";
-                        break;
-                    case 1:
-                        currentFilePath = path106;
-                        currentRoomNumber = "106";
-                        break;
-                    case 2:
-                        currentFilePath = path176;
-                        currentRoomNumber = "176";
-                        break;
-                    default:
-                        Console.WriteLine("Error found in the switch statement");
-                        break;
-                }
-                try
-                {
-                    reader = new StreamReader(File.OpenRead(currentFilePath));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                    Console.WriteLine(currentFilePath);
-                }
-                
-
-                List<string> list = new List<string>();
-
-                // Reads in header 
-                var line = reader.ReadLine();
-
-                while (!reader.EndOfStream)
-                {
-
-                    line = reader.ReadLine();
-
-                    if (!reader.EndOfStream)
-                    {
-                        line += reader.ReadLine();
-                    }
-
-                    if (!reader.EndOfStream)
-                    {
-                        line += reader.ReadLine();
-                    }
-
-                    var records = line.Split(',');
-
-                    if (records.Length == 0 || records[0] != "\"Subject\"")
-                    {
-                        foreach (var item in records)
-                        {
-                            if (item.Length > 2)
-                           {
-                                list.Add(item);                             
-                            }
-                        }
-                    }
-
-                    classes.Add(new Class
-                    {
-                        
-                        Subject = UtilityFunctions.removeOpenAndClosingQoutationMarks(list[0]),
-                        RoomNumber = currentRoomNumber,
-                        Date = DateOnly.Parse(list[1].Replace("\"", "" )),
-                        StartTime = UtilityFunctions.toShortTimeOnlyString(UtilityFunctions.removeOpenAndClosingQoutationMarks(list[2])),
-                        EndTime = UtilityFunctions.toShortTimeOnlyString(UtilityFunctions.removeOpenAndClosingQoutationMarks(list[4])),
-                        InstructorName = UtilityFunctions.removeOpenAndClosingQoutationMarks(list[10]) 
-                    }
-                    );
-
-                    list.Clear();
-                }
-                reader.Close();
-            }
-
-            classes = classes.OrderBy(o => o.RoomNumber).ThenBy(o => o.Date).ThenBy(o => o.StartTime).ToList();
-
-            return classes;
-
-        }
-
         List<Class> blankSameConsecutiveLabNames(List<Class> rows)
         {
             List<Class> newClassData = rows;
--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DisplayFormApp.utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DisplayFormApp.DataSourceHandler;
 
 namespace DisplayFormApp
 {
@@ -14,9 +16,13 @@ namespace DisplayFormApp
     {
 
        public static string ScheduleFolderPath;
+       private Form1 displayForm;
+        private DataSourceWrapper dataSource;
 
-        public Form2()
+        public Form2(ref Form1 displayForm,ref DataSourceWrapper sourceWrapper)
         {
+            this.displayForm = displayForm;
+            dataSource = sourceWrapper;
             InitializeComponent();
         }
 
@@ -32,9 +38,127 @@ namespace DisplayFormApp
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form1 f1 = new Form1(); //this is the change, code for redirect  
-            f1.ShowDialog();
+            dataSource.getClassesFunction = getClasses;
+           // displayForm = new Form1(getClasses());
             this.Close();
+            if (getClasses().Count != 0)
+            {
+                DialogResult = DialogResult.OK;
+            } else
+            {
+                DialogResult = DialogResult.Cancel;
+            }
+           // displayForm.ShowDialog();
+        }
+
+        public List<Class> getClasses()
+        {
+
+            string path = ScheduleFolderPath;
+
+            if (path == null || path.Length == 0)
+            {
+                throw new ArgumentNullException("The path needed to get schedule data is null");
+            }
+
+            string path101 = path + @"\Schedule101.CSV";
+            string path106 = path + @"\Schedule106.CSV";
+            string path176 = path + @"\Schedule176.CSV";
+
+            // Create Stream Reader
+            StreamReader reader = null;
+
+            List<Class> classes = new List<Class>();
+            List<Class> unfilteredClasses = new List<Class>();
+
+            string currentFilePath = null;
+            string currentRoomNumber = null;
+
+            for (int i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        currentFilePath = path101;
+                        currentRoomNumber = "101";
+                        break;
+                    case 1:
+                        currentFilePath = path106;
+                        currentRoomNumber = "106";
+                        break;
+                    case 2:
+                        currentFilePath = path176;
+                        currentRoomNumber = "176";
+                        break;
+                    default:
+                        Console.WriteLine("Error found in the switch statement");
+                        break;
+                }
+                try
+                {
+                    reader = new StreamReader(File.OpenRead(currentFilePath));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine(currentFilePath);
+                }
+
+
+                List<string> list = new List<string>();
+
+                // Reads in header 
+                var line = reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+
+                    line = reader.ReadLine();
+
+                    if (!reader.EndOfStream)
+                    {
+                        line += reader.ReadLine();
+                    }
+
+                    if (!reader.EndOfStream)
+                    {
+                        line += reader.ReadLine();
+                    }
+
+                    var records = line.Split(',');
+
+                    if (records.Length == 0 || records[0] != "\"Subject\"")
+                    {
+                        foreach (var item in records)
+                        {
+                            if (item.Length > 2)
+                            {
+                                list.Add(item);
+                            }
+                        }
+                    }
+
+                    classes.Add(new Class
+                    {
+
+                        Subject = UtilityFunctions.removeOpenAndClosingQoutationMarks(list[0]),
+                        RoomNumber = currentRoomNumber,
+                        Date = DateOnly.Parse(list[1].Replace("\"", "")),
+                        StartTime = UtilityFunctions.toShortTimeOnlyString(UtilityFunctions.removeOpenAndClosingQoutationMarks(list[2])),
+                        EndTime = UtilityFunctions.toShortTimeOnlyString(UtilityFunctions.removeOpenAndClosingQoutationMarks(list[4])),
+                        InstructorName = UtilityFunctions.removeOpenAndClosingQoutationMarks(list[10])
+                    }
+                    );
+
+                    list.Clear();
+                }
+                reader.Close();
+            }
+
+            classes = classes.OrderBy(o => o.RoomNumber).ThenBy(o => o.Date).ThenBy(o => o.StartTime).ToList();
+
+            return classes;
+
         }
     }
 }
