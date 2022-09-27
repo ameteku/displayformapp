d5d7fbf340ee8d08cde9fb58301ceb351333261c
Michael Ameteku <sa_ma368@uakron.edu>
2022-09-27 01:51:26 -0400

    Save progress

--- src/automatePullingData/SPAuthenticator.cs
+++ src/automatePullingData/SPAuthenticator.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Security;
+using Microsoft.SharePointOnl
 namespace DisplayFormApp.src.automatePullingData
 {
-    internal class Class1
+    internal class SPAuthenticator
     {
+        private string siteCollectionUrl = "https://uazips.sharepoint.com";
+        private string username;
+        private string password;
+        private string authToken;
+
+        SPAuthenticator(string password, string username)
+        {
+            this.password = password;
+            this.username = username;
+        }
+
+        void retrieveAuthToken()
+        {
+            ClientContext ctx = new ClientContext(siteCollectionUrl);
+
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+
+            ctx.Credentials = new SharePointOnline(username, secureString);
+        }
+        
     }
 }
