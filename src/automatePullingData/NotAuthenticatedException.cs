b8fc99854c48db35d1f5cc7d01566cb9fbf056a4
Michael Ameteku <sa_ma368@uakron.edu>
2022-09-28 18:22:22 -0400

    Add exception files

---
+++ src/automatePullingData/NotAuthenticatedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayFormApp.src.automatePullingData
+{
+    public class NotAuthenticatedException : Exception
+    {
+        public  NotAuthenticatedException(): base() { }
+        public NotAuthenticatedException(String message): base(message) { }
+    }
+}
